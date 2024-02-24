cdc0108291b0ad7a96a0dfe58eb46ce9395026ef
vivianmaria <vivig720@gmail.com>
2024-02-23 21:32:07 -0300

    atualização js

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
-function exibirTextoNaTela(tag, texto){
-let campo = document.querySelector(tag);
-campo.innerHTML = texto;
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
-
-
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; 
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        
+        exibirTextoNaTela('p', mensagemTentativas);
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'o número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'o número secreto é maior');
+        }
+        tentativas++;
+    }
 }
 
-function gerarNumeroAleatorio () {
-    return parseInt(Math.random() * 10 + 1)
-
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
 
-// testing
+
+
+
+
+
+
+
+
+
+
+
